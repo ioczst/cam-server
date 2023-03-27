59fb32188e512f8939974214063d678153fba9bb
mnhatbk20 <mnhatbk20@gmail.com>
2023-03-27 22:58:21 +0700

    change protocol

--- server.js
+++ server.js
@@ -1,42 +1,32 @@
-const RtpPacket = require("./RtpPacket")
-const KaitaiStream = require("kaitai-struct/KaitaiStream");
-
 const http = require('http')
 const UDP = require('dgram')
 const serverUDP = UDP.createSocket('udp4')
 const portUDP = 7000
 const portHTTP = 8000
 
-var timestamp
+const NMAX = 1460
+const N = NMAX-12
 
-var dataArr = []
-var bufArr =[]
-var dataSend = new Buffer.alloc(0)
+var g_ts=0
+
+var framePacketArr = []
+var frameDataArr =[]
+var frameData = new Buffer.alloc(0)
 
 
-function removeDuplicatedPacket() {
-    dataArr = dataArr.reduce((acc, current) => {
-        const x = acc.find(item => item.sq === current.sq);
-        if (!x) {
-            return acc.concat([current]);
-        } else {
-            return acc;
-        }
-    }, []);
-}
+function isFrameValid() {
 
-function isPacketValid() {
+    if (frameDataArr.length ==0)  return false
+    var startBuf = frameDataArr[0]
+    var endBuf = frameDataArr[frameDataArr.length - 1]
 
-    var startBuf = bufArr[0]
-    var endBuf = bufArr[bufArr.length - 1]
-
-    if (startBuf.length == (1460 - 12) &&
+    if (startBuf.length == (N - 6) &&
         startBuf[0] == 255 &&
         startBuf[1] == 216 &&
         startBuf[2] == 255) { // FF D8 FF          
 
         if (endBuf.length >= 2 &&
-            endBuf.length < (1460 - 12) &&
+            endBuf.length < (N - 6) &&
             endBuf[endBuf.length - 2] == 255 &&
             endBuf[endBuf.length - 1] == 217) { // FF D9
             return true
@@ -46,39 +36,54 @@ function isPacketValid() {
 
 }
 
-function setBufArr(){
-    bufArr = []
-    for (let i = 0; i < dataArr.length; i++) {
-        bufArr.push(dataArr[i].data)
+function setFrameData(){
+    frameDataArr = []
+    for (let i = 0; i < framePacketArr.length; i++) {
+        frameDataArr.push(framePacketArr[i].data)
     }
+    if(isFrameValid()) {
+        frameData = Buffer.concat(frameDataArr)
+    };
+}
+
+function removeDuplicatedPacket() {
+    framePacketArr = framePacketArr.reduce((acc, current) => {
+        const x = acc.find(item => item.seq === current.seq);
+        if (!x) {
+            return acc.concat([current]);
+        } else {
+            return acc;
+        }
+    }, []);
 }
 
 function reOrderPacket() {
-    dataArr.sort(function (a, b) {
-        if (a.sq < b.sq) return -1;
-        if (a.sq > b.sq) return 1;
+    framePacketArr.sort(function (a, b) {
+        if (a.seq < b.seq) return -1;
+        if (a.seq > b.seq) return 1;
         return 0;
     });
-    for (let i = 0; i < dataArr.length; i++) {
-        console.log("sq: ", dataArr[i].sq)
+    for (let i = 0; i < framePacketArr.length; i++) {
+        console.log("seq: ", framePacketArr[i].seq)
     }
 }
 
-serverUDP.on('message', (message, info) => {
-    var rtpPacket = new RtpPacket(new KaitaiStream(message));
+serverUDP.on('message', (packet, info) => {
+    var seq = packet.readUInt16BE(0)
+    var ts = packet.readUInt32BE(2)
+    var data = packet.subarray(6,packet.length)
 
-    if (rtpPacket.timestamp > timestamp) {
-        console.log("\ntimestamp: ", timestamp)
+    if (ts > g_ts) {
+        console.log("\ntimestamp: ", g_ts)
         removeDuplicatedPacket()
         reOrderPacket()       
-        setBufArr()
-        if(isPacketValid()) dataSend = Buffer.concat(bufArr);
-        dataArr = []
+        setFrameData()   
+        framePacketArr = []
     }
-    if (rtpPacket.timestamp >= timestamp) {
-        dataArr.push({ "sq": rtpPacket.sequenceNumber, "data": rtpPacket.data })
+    if (ts >= g_ts) {
+        framePacketArr.push({ "seq": seq, "data": data })
     }
-    timestamp = rtpPacket.timestamp
+    g_ts = ts
 
 });
 
@@ -90,13 +95,13 @@ serverUDP.bind(portUDP)
 const serverHTTP = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'multipart/x-mixed-replace; boundary=--test' })
 
-    function sendData() {
-        res.write(`--test\nContent-Type: image/jpeg\nContent-length: ${dataSend.length}\n\n`)
-        res.write(dataSend)
-        setTimeout(sendData, 50)
+    function sendFrameData() {
+        res.write(`--test\nContent-Type: image/jpeg\nContent-length: ${frameData.length}\n\n`)
+        res.write(frameData)
+        setTimeout(sendFrameData, 50)
     }
 
-    setTimeout(sendData, 50)
+    setTimeout(sendFrameData, 50)
 })
 
 serverHTTP.listen(portHTTP)
